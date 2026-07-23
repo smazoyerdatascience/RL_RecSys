e58fb30597f23c7d1ae46cf2d2bdf18143d19fec
Sylvain Mazoyer <smazoyer.datascience@gmail.com>
2026-07-23 11:54:29 +0200

    changing order in recsys_api and adding routs for clicking or buying a product

--- api/recsys_api.py
+++ api/recsys_api.py
@@ -4,16 +4,28 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def main_page():
+    return {"message": "Main page with recommandations and products selection"}
 
+@app.get("/get_product_click")
+async def get_product_click():
+    """Modify context vector with the fact a product has been clicked on. 
+    And return presentation page for the product"""
+    return {"message": "product_click"}
+
+@app.get("/get_product_buy")
+async def get_product_buy():
+    """Modify context vector with the fact a product has been bought. 
+    And return to main page with 'thanks for purchase message'"""
+    return {"message": "product_bought"}
 
 @app.get("/get_lin_ucb")
-async def get_linucb_reco():
-    """Return LinUCB recommandations list to UI"""
+async def get_lin_ucb():
+    """Return LinUCB recommandations list to UI for test purposes"""
     return {"message": "lin_ucb_reco"}
 
 @app.get("/get_content_based")
-async def get_contentbased_reco():
-    """Return Content Based recommandations list to UI"""
+async def get_content_based():
+    """Return Content Based recommandations list to UI for test purposes"""
     return {"message": "content_based_reco"}
+
